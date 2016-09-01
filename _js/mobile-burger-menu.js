a5919951915ce94e7318cdae8d15ed76b00583f1
Sebastian Perez <sebasper@gmail.com>
2016-09-01 16:23:48 -0300

    almost finished

--- _js/mobile-burger-menu.js
+++ _js/mobile-burger-menu.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
   $('.container-burger').click(function(){
     $(this).toggleClass('is-open');
     $('.menu-items-wraper').slideToggle('fast');
-
   });
   $(window).on('resize orientationChange', function(event){
     var width = $(window).width();
-
     $('.container-burger').removeClass('is-open');
     if (width > 947) {
         $('.menu-items-wraper').show();
@@ -14,6 +12,5 @@ $(document).ready(function() {
     else{
         $('.menu-items-wraper').hide();
     }
-
   });
 });
--- site/main.js
+++ site/main.js
@@ -8,11 +8,9 @@ $(document).ready(function() {
   $('.container-burger').click(function(){
     $(this).toggleClass('is-open');
     $('.menu-items-wraper').slideToggle('fast');
-
   });
   $(window).on('resize orientationChange', function(event){
     var width = $(window).width();
-
     $('.container-burger').removeClass('is-open');
     if (width > 947) {
         $('.menu-items-wraper').show();
@@ -20,6 +18,5 @@ $(document).ready(function() {
     else{
         $('.menu-items-wraper').hide();
     }
-
   });
 });
